e839da44ab47f2164e16a16e7e256922696ad38e
김보현 <bohyun8991@gmail.com>
2020-12-05 19:59:46 +0900

    [수정사항] - 깃 푸시 테스트

--- src/test/java/com/emotion/archive/UserServiceTests.java
+++ src/test/java/com/emotion/archive/UserServiceTests.java
@@ -1,11 +1,16 @@
 package com.emotion.archive;
 
+import com.emotion.archive.model.repository.UserRepository;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     @Ignore
